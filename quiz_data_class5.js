249764399064a4a32c06699d8aebc7f481adbc2e
engrkhurramali <138496924+engrkhurramali@users.noreply.github.com>
2026-05-01 18:10:09 +0500

    Add files via upload

--- quiz_data_class5.js
+++ quiz_data_class5.js
@@ -2,21 +2,165 @@ if (!window.QUIZ_DATA) window.QUIZ_DATA = {};
 Object.assign(window.QUIZ_DATA, {
     "class5_maths_u1": {
         "title": "MATHS 5 | UNIT 01",
-        "subtitle": "Numbers and Arithmetic",
-        "tsv": `Value of 5 in 523?	500	50	5	5000	A
-1000 + 500 = ?	1500	5100	1005	None	A
-What is 10 x 10?	100	20	110	1	A
-Smallest 5-digit number?	10000	99999	11111	None	A
-100 - 1 = ?	99	101	90	0	A`
+        "subtitle": "Numbers and Numeration",
+        "tsv": `What is the place value of 7 in 7,432?	7,000	700	70	7	A
+One million is equal to how many lakhs?	10 lakhs	1 lakh	100 lakhs	None	A
+What is the successor of 99,999?	100,000	99,998	10,000	None	A
+Smallest 6-digit number?	100,000	999,999	111,111	None	A
+What is 5,000 + 400 + 30 + 2?	5,432	5,043	5,403	None	A`
     },
     "class5_maths_u2": {
         "title": "MATHS 5 | UNIT 02",
-        "subtitle": "HCF and LCM",
-        "tsv": `HCF of 4 and 8?	4	2	8	1	A
-LCM of 2 and 3?	6	5	1	0	A
-Is 5 a factor of 10?	Yes	No	Maybe	None	A
-Multiple of 3?	6	4	5	None	A
-HCF stands for?	Highest Common Factor	Low Count	None	None	A`
+        "subtitle": "Operations on Large Numbers",
+        "tsv": `Sum of 10,000 and 25,000?	35,000	15,000	40,000	None	A
+Product of 100 and 50?	5,000	500	50,000	None	A
+Difference between 50,000 and 1?	49,999	50,001	40,999	None	A
+Divide 1,000 by 10?	100	10	1,000	None	A
+15,000 x 0 = ?	0	15,000	1	None	A`
+    },
+    "class5_maths_u3": {
+        "title": "MATHS 5 | UNIT 03",
+        "subtitle": "Simplification",
+        "tsv": `What does 'B' stand for in BODMAS?	Brackets	Base	Build	None	A
+10 + 5 x 2 = ?	20	30	15	None	A
+(20 - 5) / 3 = ?	5	15	10	None	A
+100 / (10 + 10) = ?	5	10	20	None	A
+Simplification uses which rule?	BODMAS	HCF	LCM	None	A`
+    },
+    "class5_maths_u4": {
+        "title": "MATHS 5 | UNIT 04",
+        "subtitle": "Factors and Multiples",
+        "tsv": `HCF of 6 and 12?	6	12	3	1	A
+LCM of 4 and 6?	12	24	10	None	A
+Is 7 a prime number?	Yes	No	Maybe	None	A
+Multiple of 9?	18	15	12	None	A
+Smallest prime number?	2	1	3	None	A`
+    },
+    "class5_maths_u5": {
+        "title": "MATHS 5 | UNIT 05",
+        "subtitle": "Unitary Method",
+        "tsv": `If 5 pens cost 50, cost of 1 pen?	10	5	15	None	A
+Cost of 10 eggs if 1 costs 2?	20	10	12	None	A
+If 3 bags weigh 15kg, 1 bag weighs?	5kg	3kg	10kg	None	A
+Unitary method finds value of?	One unit	Total units	None	None	A
+If 2 books cost 100, 5 books cost?	250	200	150	None	A`
+    },
+    "class5_maths_u6": {
+        "title": "MATHS 5 | UNIT 06",
+        "subtitle": "Fractions",
+        "tsv": `Top number of a fraction?	Numerator	Denominator	Whole	None	A
+Fraction with same denominator?	Like fractions	Unlike	Equivalent	None	A
+Is 1/2 greater than 1/4?	Yes	No	Equal	None	A
+What is 2/4 simplified?	1/2	1/4	2/2	None	A
+Fraction where numerator > denominator?	Improper	Proper	Mixed	None	A`
+    },
+    "class5_maths_u7": {
+        "title": "MATHS 5 | UNIT 07",
+        "subtitle": "Operations on Fractions",
+        "tsv": `1/4 + 1/4 = ?	1/2	2/8	1/8	None	A
+1/2 - 1/4 = ?	1/4	1/2	0	None	A
+Product of 1/2 and 1/2?	1/4	1	1/2	None	A
+Reciprocal of 2/3?	3/2	2/3	1	None	A
+Divide 1/2 by 2?	1/4	1	2	None	A`
+    },
+    "class5_maths_u8": {
+        "title": "MATHS 5 | UNIT 08",
+        "subtitle": "Decimals",
+        "tsv": `0.5 is same as?	1/2	1/5	5/100	None	A
+Which is larger: 0.1 or 0.01?	0.1	0.01	Equal	None	A
+Sum of 0.2 and 0.3?	0.5	0.05	5	None	A
+0.1 x 10 = ?	1	0.1	10	None	A
+Decimal for 1/4?	0.25	0.5	0.75	None	A`
+    },
+    "class5_maths_u9": {
+        "title": "MATHS 5 | UNIT 09",
+        "subtitle": "Ratio and Proportion",
+        "tsv": `Ratio of 2 to 4?	1:2	2:1	4:2	None	A
+Symbol for ratio?	:	/	+	None	A
+Are 1:2 and 2:4 proportional?	Yes	No	Maybe	None	A
+Ratio compares how many quantities?	Two or more	One	None	None	A
+Equality of two ratios is?	Proportion	Fraction	Decimal	None	A`
+    },
+    "class5_maths_u10": {
+        "title": "MATHS 5 | UNIT 10",
+        "subtitle": "Percentages",
+        "tsv": `Percent means per?	100	10	1000	1	A
+50% of 100?	50	25	75	None	A
+1/4 as a percentage?	25%	50%	75%	None	A
+Symbol for percentage?	%	$	#	None	A
+100% is equal to?	Whole	Half	Zero	None	A`
+    },
+    "class5_maths_u11": {
+        "title": "MATHS 5 | UNIT 11",
+        "subtitle": "Line and Angles",
+        "tsv": `Angle of 90 degrees?	Right angle	Acute	Obtuse	None	A
+Angle less than 90?	Acute angle	Right	Obtuse	None	A
+Straight line angle?	180 degrees	90	360	None	A
+Line with one endpoint?	Ray	Line segment	Line	None	A
+Tool to measure angles?	Protractor	Ruler	Compass	None	A`
+    },
+    "class5_maths_u12": {
+        "title": "MATHS 5 | UNIT 12",
+        "subtitle": "Circles, Triangles and Quadrilaterals",
+        "tsv": `Sum of angles in a triangle?	180 degrees	90	360	None	A
+Quadrilateral has how many sides?	4	3	5	6	A
+Center to boundary of circle?	Radius	Diameter	Chord	None	A
+Triangle with 3 equal sides?	Equilateral	Isosceles	Scalene	None	A
+Longest chord of a circle?	Diameter	Radius	Arc	None	A`
+    },
+    "class5_maths_u13": {
+        "title": "MATHS 5 | UNIT 13",
+        "subtitle": "Perimeter, Area and Volume",
+        "tsv": `Perimeter of square (side s)?	4s	s*s	2s	None	A
+Area of rectangle?	Length x Width	Sum of sides	2(L+W)	None	A
+Space occupied by 3D object?	Volume	Area	Perimeter	None	A
+Unit of area?	Square units	Cubic units	Units	None	A
+Perimeter of triangle with sides 3, 4, 5?	12	7	9	None	A`
+    },
+    "class5_maths_u14": {
+        "title": "MATHS 5 | UNIT 14",
+        "subtitle": "Measurement (I)",
+        "tsv": `Standard unit of length?	Meter	Gram	Liter	None	A
+1 kilometer = ?	1000 meters	100 meters	10 meters	None	A
+1 kilogram = ?	1000 grams	100 grams	10 grams	None	A
+Unit for measuring weight?	Gram	Meter	Liter	None	A
+Length of a pencil measured in?	Centimeters	Kilometers	Meters	None	A`
+    },
+    "class5_maths_u15": {
+        "title": "MATHS 5 | UNIT 15",
+        "subtitle": "Measurement (II)",
+        "tsv": `How many seconds in 1 minute?	60	30	100	None	A
+Unit of temperature?	Celsius	Gram	Meter	None	A
+Water freezes at?	0 degrees C	100 degrees C	32 degrees C	None	A
+1 hour = ?	60 minutes	100 minutes	24 minutes	None	A
+Standard unit of capacity?	Liter	Meter	Gram	None	A`
+    },
+    "class5_maths_u16": {
+        "title": "MATHS 5 | UNIT 16",
+        "subtitle": "Money",
+        "tsv": `Currency of Pakistan?	Rupee	Dollar	Pound	None	A
+100 Paisa is equal to?	1 Rupee	10 Rupees	5 Rupees	None	A
+Adding 5.50 and 4.50?	10.00	9.00	11.00	None	A
+Profit = ?	Selling Price - Cost Price	CP - SP	Price	None	A
+Loss occurs when?	CP > SP	SP > CP	CP = SP	None	A`
+    },
+    "class5_maths_u17": {
+        "title": "MATHS 5 | UNIT 17",
+        "subtitle": "Data Handling",
+        "tsv": `Representation of data using bars?	Bar graph	Pictograph	Line graph	None	A
+Average is also called?	Mean	Median	Mode	None	A
+Data in form of pictures?	Pictograph	Bar graph	Table	None	A
+Tally marks group in?	5s	10s	2s	None	A
+Collecting information is?	Data collection	Graphing	Solving	None	A`
+    },
+    "class5_maths_u18": {
+        "title": "MATHS 5 | UNIT 18",
+        "subtitle": "Introduction of Algebra",
+        "tsv": `Letter used to represent number?	Variable	Constant	Sign	None	A
+In 2x, 'x' is?	Variable	Coefficient	None	None	A
+Expression with '=' sign?	Equation	Formula	Term	None	A
+If x + 2 = 5, x = ?	3	7	5	None	A
+Algebra uses?	Letters and Numbers	Only numbers	None	None	A`
     },
     "class5_science_u1": {
         "title": "SCIENCE 5 | UNIT 01",
